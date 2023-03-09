5d34b039ac4093e0fce01833437c5322e97a3339
NotOven <97773165+NotOven@users.noreply.github.com>
2023-03-09 17:33:37 -0500

    Update Sprint.lua

--- Misc/Sprint.lua
+++ Misc/Sprint.lua
@@ -1,8 +1,22 @@
 local InputService = game:GetService("UserInputService")
 local plr = game:GetService('Players').LocalPlayer
-local KeyCode = Enum.KeyCode.LeftShift
+local KeyCode = Enum.KeyCode.LeftControl
 local sprinting = false
 local NormSpeed = plr.Character:FindFirstChildOfClass('Humanoid').WalkSpeed
+
+--Notif To Switch Sprint Key In Case LeftControl Is Already Used
+local Bindable = Instance.new("BindableFunction")
+function Switch(Response)
+	if Response == "Yes" then
+		local KeyCode = Enum.KeyCode.LeftShift
+	elseif Response == "No" then
+		local KeyCode = Enum.KeyCode.LeftControl
+	end
+	Bindable:Destroy()
+end
+
+    Bindable.OnInvoke = Switch
+    game.StarterGui:SetCore("SendNotification",{Title = "Switch Sprint Key To Left Shift?";Text = "";Duration = 9e9;Callback = Bindable,Button1 = "Yes",Button2 = "No"})
 
 InputService.InputBegan:Connect(function(input)
 	if input.KeyCode == KeyCode then
